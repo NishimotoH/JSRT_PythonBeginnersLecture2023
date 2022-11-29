d76975ade0d8827c6dbf66149e6490ce03fb2d73
H. Nishimoto <50865611+NishimotoH@users.noreply.github.com>
2022-11-29 11:09:49 +0900

    Add files via upload

---
+++ timeEval.py
@@ -0,0 +1,87 @@
+import pydicom
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+# Read DICOM data
+filename = pydicom.data.get_testdata_file("CT_small.dcm")
+dc = pydicom.dcmread(filename)
+
+# Thresholding -> imBone
+threshold1 = 1400
+
+# Original
+time_sta = time.time()
+imBone1 = np.zeros(dc.pixel_array.shape)
+for x in range(0, dc.pixel_array.shape[0]):
+    for y in range(0, dc.pixel_array.shape[1]):
+        if dc.pixel_array[x, y] >= threshold1:
+            imBone1[x, y] = 1
+        else:
+            imBone1[x, y] = 0
+time_end = time.time()
+tim = time_end- time_sta
+print(tim)
+
+time.sleep(1.0)
+
+# Eliminate "else"
+time_sta = time.time()
+imBone2 = np.zeros(dc.pixel_array.shape)
+for x in range(0, dc.pixel_array.shape[0]):
+    for y in range(0, dc.pixel_array.shape[1]):
+        if dc.pixel_array[x, y] >= threshold1:
+            imBone2[x, y] = 1
+time_end = time.time()
+tim = time_end- time_sta
+print(tim)
+
+time.sleep(1.0)
+
+
+# No use "if" and "for"
+time_sta = time.time()
+imBone3 = (dc.pixel_array > threshold1)
+time_end = time.time()
+tim = time_end- time_sta
+print(tim)
+
+
+# Big size list (10,000 x 10,000 matrix, random)
+bsl = np.random.randint(0, 100, (5000, 5000))
+threshold2 = 500
+
+# Original
+time_sta = time.time()
+bsl1 = np.zeros(bsl.shape)
+for x in range(0, bsl.shape[0]):
+    for y in range(0, bsl.shape[1]):
+        if bsl[x, y] >= threshold2:
+            bsl1[x, y] = 1
+        else:
+            bsl1[x, y] = 0
+time_end = time.time()
+tim = time_end- time_sta
+print("Big size list, original: " + str(tim))
+
+
+# Eliminate "else"
+time_sta = time.time()
+bsl2 = np.zeros(bsl.shape)
+for x in range(0, bsl.shape[0]):
+    for y in range(0, bsl.shape[1]):
+        if bsl[x, y] >= threshold2:
+            bsl2[x, y] = 1
+time_end = time.time()
+tim = time_end- time_sta
+print("Big size list, No \"else\": " + str(tim))
+
+time.sleep(1.0)
+
+
+# No use "if" and "for"
+time_sta = time.time()
+bsl3 = (bsl > threshold2)
+time_end = time.time()
+tim = time_end- time_sta
+print("Big size list, No \"if\" and \"for\": " + str(tim))
