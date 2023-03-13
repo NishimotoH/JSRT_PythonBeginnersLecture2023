17d8ffadef488b651eda72d1d3be32832b507bd3
H. Nishimoto <50865611+NishimotoH@users.noreply.github.com>
2023-03-13 23:19:19 +0900

    20230313-2319

--- Fig6_threshouldedImage.py
+++ Fig6_threshouldedImage.py
@@ -7,12 +7,11 @@ filename = pydicom.data.get_testdata_file("CT_small.dcm")
 dc = pydicom.dcmread(filename)
 
 # Thresholding -> imBone
-threshould = 1400
-print(dc.pixel_array.shape)
+threshold = 1400
 imBone = np.zeros(dc.pixel_array.shape)
 for x in range(0, dc.pixel_array.shape[0]):
     for y in range(0, dc.pixel_array.shape[1]):
-        if dc.pixel_array[x, y] > threshould:
+        if dc.pixel_array[x, y] >= threshold:
             imBone[x, y] = 1
         else:
             imBone[x, y] = 0
